7483936f427209fcb870b46851dc8b8e8a7782c6
Coding With Kien <duongtrungkien.03052003@gmail.com>
2024-09-19 11:22:21 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-green-300 flex item-center justify-center">
+        App
+      </Text>
+    </View>
+  );
+};
+
+export default App;
